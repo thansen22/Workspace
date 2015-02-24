7f16b46f1fd51bb2acd44864ef1bd527e2944b79
thansen22 <thansen22@hotmail.com>
2015-02-24 15:50:13 -0800

    V3.5 with more jpg types to consider and file delete

--- Nokia1020CopyHiResPics/src/Jmtp.java
+++ Nokia1020CopyHiResPics/src/Jmtp.java
@@ -19,10 +19,10 @@ import jmtp.*;
 
 public class Jmtp {
 	// preferences/custom values
-	final static String destination = "Z:\\Shared Pictures\\From Timmay\\Camera roll";
-	final static String deviceGuid = "6ac27878-a6fa-4155-ba85-f98f491d4f33"; // get this by iterating through manager.getDevices().toString()		
+	final static String destination = "C:/Users/timha_000/test"; //"Z:/Shared Pictures/From Timmay/Camera roll";
+	final static String deviceId = "vid_0421&pid_0661&mi_00#7&1bb7d0ae&0&0000"; // get this by iterating through manager.getDevices().toString()		
 	final static BigInteger spaceCleanupThresholdGB = BigInteger.valueOf(3); 
-	final static Boolean cleanupFirst = true;
+	final static Boolean cleanupFirst = false;
 	final static int cleanupMoviePercentage = 25;
 	
 	// constants
@@ -31,19 +31,30 @@ public class Jmtp {
 	final static String jmtpLib = "jmtp";
 	final static String primaryFolder = "Pictures";
 	final static String secondaryFolder = "Camera Roll";
-	final static String hiResPicsFilter = "pro__highres.jpg";
-	final static String smartPicsFilter = "smart.jpg";
-	final static String smartNarFilter = "smart.nar";
-	final static String movieFilter = "pro.mp4";
+	final static String allPicsFilterPattern = ".jpg";
+	final static String hiResPicsFilterPattern = "__highres.jpg";
+	final static String smartPicsFilterPattern = "smart.jpg";
+	final static String smartNarFilterPattern = "smart.nar";
+	final static String movieFilterPattern = "pro.mp4";
 	
 	// "global" values
 	static Logger errorLog;
 	static Logger successLog;
 	static PortableDevice device;
+	static FilenameFilter allPicsFilter = new FilenameFilter() {
+		public boolean accept(File dir, String name) {
+			String lowercaseName = name.toLowerCase();
+			if (lowercaseName.contains(allPicsFilterPattern)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+    };
 	static FilenameFilter hiResFilter = new FilenameFilter() {
 		public boolean accept(File dir, String name) {
 			String lowercaseName = name.toLowerCase();
-			if (lowercaseName.contains(hiResPicsFilter)) {
+			if (lowercaseName.contains(hiResPicsFilterPattern)) {
 				return true;
 			} else {
 				return false;
@@ -53,7 +64,7 @@ public class Jmtp {
 	static FilenameFilter smartFilter = new FilenameFilter() {
 		public boolean accept(File dir, String name) {
 			String lowercaseName = name.toLowerCase();
-			if (lowercaseName.contains(smartPicsFilter)) {
+			if (lowercaseName.contains(smartPicsFilterPattern)) {
 				return true;
 			} else {
 				return false;
@@ -63,7 +74,7 @@ public class Jmtp {
 	static FilenameFilter narFilter = new FilenameFilter() {
 		public boolean accept(File dir, String name) {
 			String lowercaseName = name.toLowerCase();
-			if (lowercaseName.contains(smartNarFilter)) {
+			if (lowercaseName.contains(smartNarFilterPattern)) {
 				return true;
 			} else {
 				return false;
@@ -73,7 +84,7 @@ public class Jmtp {
 	static FilenameFilter movFilter = new FilenameFilter() {
 		public boolean accept(File dir, String name) {
 			String lowercaseName = name.toLowerCase();
-			if (lowercaseName.contains(movieFilter)) {
+			if (lowercaseName.contains(movieFilterPattern)) {
 				return true;
 			} else {
 				return false;
@@ -87,7 +98,7 @@ public class Jmtp {
        
         for (PortableDevice dev : manager.getDevices())
         {
-        	if (dev.toString().contains(deviceGuid))
+        	if (dev.toString().contains(deviceId))
         	{
         		device = dev;
         	}
@@ -124,16 +135,71 @@ public class Jmtp {
                     				cleanup(storage, folder);
                     			}
                     			
-                    			ArrayList<PortableDeviceObject> newFiles = new ArrayList<>();                    			
-                    			// add the hiRes pics
-                    			String lastHiResCopied = findLastFileCopied(hiResFilter);              			                    			
-                    			newFiles.addAll(findFilesOnDevice(folder, hiResPicsFilter, lastHiResCopied));                    			
-                    			// add the smart pics
-                    			String lastSmartCopied = findLastFileCopied(smartFilter);              			                    			
-                    			newFiles.addAll(findFilesOnDevice(folder, smartPicsFilter, lastSmartCopied));                    			
-                    			// add the movies
-                    			String lastMovieCopied = findLastFileCopied(movFilter);              			                    			
-                    			newFiles.addAll(findFilesOnDevice(folder, movieFilter, lastMovieCopied));                    			
+                    			ArrayList<PortableDeviceObject> originalFiles = new ArrayList<>();
+                    			originalFiles.addAll(findFilesOnDevice(folder, allPicsFilterPattern, ""));
+                    			
+                    			ArrayList<PortableDeviceObject> newFiles = new ArrayList<>();
+                    			newFiles.addAll(findFilesOnDevice(folder, allPicsFilterPattern, ""));
+                    			
+//                    			System.out.println("All pics before reduction: " + newFiles.size());
+                    			int allFilesIndex = 0;                    			
+                    			for(PortableDeviceObject file : originalFiles)
+                    			{                    				
+                    				// we will copy the .jpg files unless there's a __highres file with the same name and the same creation date.
+                    				// the other types of .jpg files can be those taken that are from smart capture, office lens, front facing camera, etc... 
+                    				if (!file.getOriginalFileName().toLowerCase().contains(hiResPicsFilterPattern))
+                    				{
+                    					// check to see if it has a highres counterpart
+                    					String possibleHiResFileName = file.getOriginalFileName().substring(0, file.getOriginalFileName().indexOf(".jpg")).concat(hiResPicsFilterPattern);
+                    					
+                    					// unfortunately we have to go through the entire collection to see if it exists as order could be anything, however, since it's most likely the very next object... doing a hack shortcut for speed...                    					
+                    					if (allFilesIndex + 1 < originalFiles.size() && originalFiles.get(allFilesIndex + 1).getOriginalFileName().equals(possibleHiResFileName))
+                    					{
+                    						if (originalFiles.get(allFilesIndex + 1).getDateAuthored() == null)
+                    						{
+                    							System.out.println(originalFiles.get(allFilesIndex + 1).getOriginalFileName());                    							
+                    						}
+                    						//System.out.println("found alternate high res file: " + possibleHiResFileName + " still need to check creation date...");
+                    						if (originalFiles.get(allFilesIndex + 1).getDateAuthored().equals(file.getDateAuthored()))
+                    						{
+                    							System.out.println("remove file");
+                    							newFiles.remove(file);
+                    						}                    						                    						
+                    					}
+                    					else
+                    					{
+                    						// didn't find it, so now we have to go through everything.  This hit appears to be only for the other file types, and possibly some other user manipulation, just don't want to assume anything...
+                    						for (PortableDeviceObject secondFileCheck : originalFiles)
+                    						{
+                    							if (secondFileCheck.getOriginalFileName().equals(possibleHiResFileName))
+                    							{
+                    								System.out.println("found alternate high res file in a different location than the next iterator: " + possibleHiResFileName + ". Remove this else block if this message is never displayed as it's slow...");
+                    								newFiles.remove(secondFileCheck);
+                    							}
+                    						}                    						
+                    					}
+                    				}
+                    				allFilesIndex++;
+                    			}
+//                    			System.out.println("All pics after reduction: " + newFiles.size());
+//                    			for (PortableDeviceObject file : newFiles)
+//                    			{
+//                    				System.out.println(file.getOriginalFileName());
+//                    			}
+                    			
+//
+//                    			// add the hiRes pics
+//                    			String lastHiResCopied = findLastFileCopied(hiResFilter, hiResPicsFilterPattern);              			                    			
+//                    			newFiles.addAll(findFilesOnDevice(folder, hiResPicsFilterPattern, lastHiResCopied));                    			
+//                    			// add the smart pics
+//                    			String lastSmartCopied = findLastFileCopied(smartFilter, smartPicsFilterPattern);              			                    			
+//                    			newFiles.addAll(findFilesOnDevice(folder, smartPicsFilterPattern, lastSmartCopied));                    			
+//                    			// add the movies
+//                    			String lastMovieCopied = findLastFileCopied(movFilter, movieFilterPattern);              			                    			
+//                    			newFiles.addAll(findFilesOnDevice(folder, movieFilterPattern, lastMovieCopied));                    			
+                    			
+                    			//TODO: temp here to play around with the new pics logic
+                    			System.exit(0);
                     			
                     			if (newFiles.size() == 0)
                     			{
@@ -176,79 +242,90 @@ public class Jmtp {
 			freeSpace = storage.getFreeSpace();
 			if (freeSpace.compareTo(spaceCleanupThreshold) < 0)
 			{
-				System.out.println("Still not enough. You now have " + freeSpace.divide(megaByte) + "MB left with a threshold of " + spaceCleanupThreshold.divide(megaByte) + "MB");
-				
-				ArrayList<PortableDeviceObject> movFiles = findFilesOnDevice(folder, movieFilter, "");
-				if (movFiles.size() > 0)
+				System.out.println("Still not enough. You now have " + freeSpace.divide(megaByte) + "MB left with a threshold of " + spaceCleanupThreshold.divide(megaByte) + "MB");				
+				deleteMovieFiles(folder);
+			}
+		}
+	}
+
+	private static void deleteMovieFiles(PortableDeviceObject folder) {
+		ArrayList<PortableDeviceObject> movFiles = findFilesOnDevice(folder, movieFilterPattern, "");
+		if (movFiles.size() > 0)
+		{
+			BigInteger totalMovieSpace = BigInteger.ZERO;				    
+			BigInteger partialMovieSpace = BigInteger.ZERO;
+			int filesToDelete = cleanupMoviePercentage * movFiles.size() / 100;
+			int filesToDeleteCounter = filesToDelete;
+			for (PortableDeviceObject movie : movFiles)
+			{
+				BigInteger movSize = movie.getSize();
+				totalMovieSpace = totalMovieSpace.add(movSize);
+				if (filesToDeleteCounter > 0)
 				{
-					BigInteger totalMovieSpace = BigInteger.ZERO;				    
-					BigInteger partialMovieSpace = BigInteger.ZERO;
-					int filesToDelete = cleanupMoviePercentage * movFiles.size() / 100;
-					int filesToDeleteCounter = filesToDelete;
-					for (PortableDeviceObject movie : movFiles)
+					partialMovieSpace = partialMovieSpace.add(movSize);
+					filesToDeleteCounter--;
+				}
+			}
+			System.out.println("Your " + movFiles.size() + " videos are taking up a total of " + totalMovieSpace.divide(megaByte) + "MBs");
+			System.out.println("Your preferences are to delete " + cleanupMoviePercentage + "% of your total videos, this translates into " + filesToDelete + " and a total space of " + partialMovieSpace.divide(megaByte) + "MBs.");
+			
+			int confirm = JOptionPane.NO_OPTION;
+			do
+			{
+				String userInput = JOptionPane.showInputDialog("You can use this dialog to delete more or less files modifying the deletion of: " + partialMovieSpace.divide(megaByte) + "MBs of videos. Modifying the value will recalculate and show a confirmation. Click cancel to skip deleting videos", filesToDelete);
+				if (userInput == null)
+				{
+					System.out.println("User cancelled video deletion");
+					break;
+				}
+				else 				
+			    {	
+					filesToDelete = Integer.parseInt(userInput);
+					if (filesToDelete > 0 && filesToDelete <= movFiles.size())
 					{
-						BigInteger movSize = movie.getSize();
-						totalMovieSpace = totalMovieSpace.add(movSize);
-						if (filesToDeleteCounter > 0)
-						{
-							partialMovieSpace = partialMovieSpace.add(movSize);
-							filesToDeleteCounter--;
-						}
-					}
-					System.out.println("Your " + movFiles.size() + " videos are taking up a total of " + totalMovieSpace.divide(megaByte) + "MBs");
-					System.out.println("Your preferences are to delete " + cleanupMoviePercentage + "% of your total videos, this translates into " + filesToDelete + " and a total space of " + partialMovieSpace.divide(megaByte) + "MBs.");
-					
-					int confirm = JOptionPane.NO_OPTION;
-					do
-					{
-						String userInput = JOptionPane.showInputDialog("You can use this dialog to delete more or less files modifying the deletion of: " + partialMovieSpace.divide(megaByte) + "MBs of videos. Modifying the value will recalculate and show a confirmation. Click cancel to skip deleting videos", filesToDelete);
-						if (userInput == null)
-						{
-							System.out.println("User cancelled video deletion");
-							break;
-						}
-						else 				
-					    {	
-							filesToDelete = Integer.parseInt(userInput);
-							if (filesToDelete > 0 && filesToDelete <= movFiles.size())
-							{
-								partialMovieSpace = BigInteger.ZERO;
-								for (int i = 0; i < filesToDelete; i++)
-								{
-									partialMovieSpace = partialMovieSpace.add(movFiles.get(i).getSize());
-								}
-								confirm = JOptionPane.showConfirmDialog(null, "Going to delete " + userInput + " videos now freeing up " + partialMovieSpace.divide(megaByte) + "MBs... Confirm?", "Delete Confirmation", JOptionPane.YES_NO_OPTION);
-							}
-					    }
-					} while (confirm == JOptionPane.NO_OPTION);
-					if (confirm == JOptionPane.YES_OPTION)
-					{
-						System.out.println("Start the deleting of videos!");
-						int vidFilesDeleted = 0;
+						partialMovieSpace = BigInteger.ZERO;
 						for (int i = 0; i < filesToDelete; i++)
 						{
-							System.out.print(movFiles.get(i).getOriginalFileName() + ", ");
-//							String vidFileName = movFiles.get(i).getOriginalFileName();
-//							try
-//							{
-//								movFiles.get(i).delete();
-//								log(successLog, "success", "Deleted " + ++vidFilesDeleted + " of " + movFiles.size() + " files: " + vidFileName);
-//							} 							
-//							catch (Exception e)
-//							{
-//								e.printStackTrace();
-//								log(errorLog, "error", "Failed to import file " + vidFileName);
-//							}
-						}	
-						System.out.println("");
+							partialMovieSpace = partialMovieSpace.add(movFiles.get(i).getSize());
+						}
+						confirm = JOptionPane.showConfirmDialog(null, "Going to delete " + userInput + " videos now freeing up " + partialMovieSpace.divide(megaByte) + "MBs... Confirm?", "Delete Confirmation", JOptionPane.YES_NO_OPTION);
 					}
-				}
+			    }
+			} while (confirm == JOptionPane.NO_OPTION);
+			if (confirm == JOptionPane.YES_OPTION)
+			{
+				System.out.println("Start the deleting of videos!");
+				int vidFilesDeleted = 0;
+				int totalFilesToDelete = movFiles.size();
+				for (int i = 0; i < filesToDelete; i++)
+				{
+					String vidFileName = movFiles.get(i).getOriginalFileName();
+					File checkForFile = new File(destination, movFiles.get(i).getOriginalFileName());
+					if (checkForFile.exists())
+					{
+						try
+						{
+							//movFiles.get(i).delete();
+							log(successLog, "success", "Deleted " + ++vidFilesDeleted + " of " +totalFilesToDelete + " files: " + vidFileName);
+						} 							
+						catch (Exception e)
+						{
+							e.printStackTrace();
+							log(errorLog, "error", "Failed to import file " + vidFileName);
+						}
+					}
+					else
+					{
+						log(errorLog, "error", "File " + checkForFile + " not found. You must first manually copy this file to the destination folder or manually delete this file from your phone.");
+						totalFilesToDelete--;
+					}
+				}	
 			}
 		}
 	}
 
 	private static void deleteNarFiles(PortableDeviceObject folder) {
-		ArrayList<PortableDeviceObject> narFiles = findFilesOnDevice(folder, smartNarFilter, "");
+		ArrayList<PortableDeviceObject> narFiles = findFilesOnDevice(folder, smartNarFilterPattern, "");
 		if (narFiles.size() > 0)
 		{
 			int userInput = JOptionPane.showConfirmDialog(null, "Found " + narFiles.size() + " .nar file(s). These are only used for on the camera for smart capture.  Would you like to delete them?", "Delete .nar files?", JOptionPane.YES_NO_OPTION);
@@ -296,7 +373,7 @@ public class Jmtp {
 		return filesOnDevice;
 	}
 
-	private static String findLastFileCopied(FilenameFilter filter) {
+	private static String findLastFileCopied(FilenameFilter filter, String filePattern) {
 		// Find the last picture we copied over                    			
 		File dst = new File(destination);
 		String[] dstPicNames = dst.list(filter);		
@@ -312,7 +389,7 @@ public class Jmtp {
 		{
 			// Use joption pane if this ever runs out of debug...
 			//String userInput = JOptionPane.showInputDialog("No previous pictures found in " + destination + " do you wish to proceed? (Y/N):");
-			int userInput = JOptionPane.showConfirmDialog(null, "No previous files of type " + filter.toString() + " found in " + destination + " do you wish to proceed?", "New destination or new files found?", JOptionPane.YES_NO_OPTION);
+			int userInput = JOptionPane.showConfirmDialog(null, "No previous files with the pattern *" + filePattern + " found in " + destination + " do you wish to proceed?", "New destination or new files found?", JOptionPane.YES_NO_OPTION);
 			if (userInput == JOptionPane.NO_OPTION)
 		    {
 		    	System.exit(0);
